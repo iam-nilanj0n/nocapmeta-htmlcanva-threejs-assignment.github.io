cdce00c5007b15f7ff2f91287979fa21337984c0
iam-nilanj0n <98917336+iam-nilanj0n@users.noreply.github.com>
2023-08-04 17:42:17 +0530

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const controls = new OrbitControls( camera, renderer.domElement );
 const loader = new GLTFLoader();
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
